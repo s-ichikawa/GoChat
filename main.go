12b01d5d71dc66b25c210ab43fdab207c6fb5bd9
shingo.ichikawa <shingo.ichikawa@inte.co.jp>
2016-05-10 07:31:55 +0900

    1.3

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "sync"
     "text/template"
     "path/filepath"
+    "flag"
 )
 
 type templateHandler struct {
@@ -18,17 +19,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     t.once.Do(func() {
         t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
     })
-    t.templ.Execute(w, nil)
+    t.templ.Execute(w, r)
 }
 
 func main() {
+    var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+    flag.Parse()
     r := newRoom()
     http.Handle("/", &templateHandler{filename: "chat.html"})
     http.Handle("/room", r)
     // チャットルームを開始
     go r.run()
     // Webサーバを起動
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+    log.Println("Webサーバを開始します。ポート:", *addr)
+    if err := http.ListenAndServe(*addr, nil); err != nil {
         log.Fatal("ListenAndServe:", err)
     }
 }
