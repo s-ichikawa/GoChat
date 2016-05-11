d5e9621796e1741f644662d9b962c00fc04d2035
shingo.ichikawa <shingo.ichikawa@inte.co.jp>
2016-05-11 21:24:25 +0900

    1.4

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ func main() {
     var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
     flag.Parse()
     r := newRoom()
+    //r.tracer = trace.New(os.Stdout)
     http.Handle("/", &templateHandler{filename: "chat.html"})
     http.Handle("/room", r)
     // チャットルームを開始
