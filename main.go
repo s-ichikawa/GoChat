092ee4a473e1d54426a1d172fe31750cbdeaed1a
shingo.ichikawa <shingo.ichikawa@inte.co.jp>
2016-05-26 22:30:13 +0900

    2.5 + ini

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
     "text/template"
     "path/filepath"
     "flag"
+    "github.com/stretchr/gomniauth"
+    "github.com/stretchr/gomniauth/providers/facebook"
+    "github.com/stretchr/gomniauth/providers/github"
+    "github.com/stretchr/gomniauth/providers/google"
+    "github.com/stretchr/signature"
+    "github.com/vaughan0/go-ini"
 )
 
 type templateHandler struct {
@@ -25,6 +31,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
     var addr = flag.String("host", ":8080", "アプリケーションのアドレス")
     flag.Parse()
+
+    file, _ := ini.LoadFile(".ini")
+    facebook_id, _ := file.Get("FaceBook", "id")
+    facebook_callback, _ := file.Get("FaceBook", "callback")
+    github_id, _ := file.Get("GitHub", "id")
+    github_secret, _ := file.Get("GitHub", "secret")
+    github_callback, _ := file.Get("GitHub", "callback")
+    google_id, _ := file.Get("Google", "id")
+    google_secret, _ := file.Get("Google", "secret")
+    google_callback, _ := file.Get("Google", "callback")
+    gomniauth.SetSecurityKey(signature.RandomKey(64))
+    gomniauth.WithProviders(
+        facebook.New(facebook_id, "", facebook_callback),
+        github.New(github_id, github_secret, github_callback),
+        google.New(google_id, google_secret, google_callback),
+    )
     r := newRoom()
     //r.tracer = trace.New(os.Stdout)
     http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
