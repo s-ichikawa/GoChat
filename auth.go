2722a49964ba7dc59eb477e3b3073eff724f61b1
shingo.ichikawa <ichikawa.shingo.0829@gmail.com>
2016-06-15 22:18:02 +0900

    3.3.6.1

--- auth.go
+++ auth.go
@@ -10,6 +10,20 @@ import (
     "crypto/md5"
     "io"
 )
+import gomniauthcommon "github.com/stretchr/gomniauth/common"
+
+type ChatUser interface {
+    UniqueID() string
+    AvatarURL() string
+}
+type chatUser struct {
+    gomniauthcommon.User
+    uniqueID string
+}
+
+func (u chatUser) UniqueID() string {
+    return u.uniqueID
+}
 
 type authHandler struct {
     next http.Handler
