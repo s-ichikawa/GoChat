a54a983372dee423cda936e74fba578b78470ae0
shingo.ichikawa <ichikawa.shingo.0829@gmail.com>
2016-06-19 20:57:05 +0900

    リファクタリング

--- avatar.go
+++ avatar.go
@@ -12,9 +12,7 @@ type Avatar interface {
     GetAvatarURL(u ChatUser) (string, error)
 }
 
-type AuthAvatar struct{}
 type TryAvatars []Avatar
-
 func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
     for _, avatar := range a {
         if url, err := avatar.GetAvatarURL(u); err == nil {
@@ -24,8 +22,8 @@ func (a TryAvatars) GetAvatarURL(u ChatUser) (string, error) {
     return "", ErrNoAvatarURL
 }
 
+type AuthAvatar struct{}
 var UseAuthAvatar AuthAvatar
-
 func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
     url := u.AvatarURL()
     if url != "" {
@@ -35,17 +33,13 @@ func (_ AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
 }
 
 type GravatarAvatar struct{}
-
 var UseGravatar GravatarAvatar
-
 func (_ GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
     return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
 }
 
 type FileSystemAvatar struct{}
-
 var UseFileSystemAvatar FileSystemAvatar
-
 func (_ FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
     if files, err := ioutil.ReadDir("avatars"); err == nil {
         for _, file := range files {
